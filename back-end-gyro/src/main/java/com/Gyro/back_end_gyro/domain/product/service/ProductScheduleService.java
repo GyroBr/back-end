73c1454e55051598f18c4a47266113ae09c71986
EduardoMeloDeOliveira <eduardo.moliveira@sptech.school>
2025-05-11 13:09:32 -0300

    add new infos ao .env, cria implementação para envio de email, porém ainda sem funcionar dado a um erro no JavaMailSender, irei investigar, até o próximo commit,feliz dia das mães

--- back-end-gyro/src/main/java/com/Gyro/back_end_gyro/domain/product/service/ProductScheduleService.java
+++ back-end-gyro/src/main/java/com/Gyro/back_end_gyro/domain/product/service/ProductScheduleService.java
@@ -1,5 +1,6 @@
 package com.Gyro.back_end_gyro.domain.product.service;
 
+import com.Gyro.back_end_gyro.domain.email.service.EmailService;
 import com.Gyro.back_end_gyro.domain.product.entity.Product;
 import com.Gyro.back_end_gyro.domain.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
@@ -7,36 +8,62 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
-import java.util.List;
+import java.util.*;
 
 @Component
 @RequiredArgsConstructor
 public class ProductScheduleService {
 
     private final ProductRepository productRepository;
+    private final EmailService emailService;
+
 
     @Scheduled(fixedRate = 30000)
     public void setExpiredProduct() {
-
         List<Product> products = productRepository.findAll();
+        Map<String, List<Product>> expiredProductsByEmail = new HashMap<>();
 
         for (Product product : products) {
-
-            if(product.getExpiresAt().equals(LocalDate.now())) {
+            if (product.getExpiresAt().equals(LocalDate.now())) {
                 product.setIsExpiredProduct(true);
+                String email = product.getCompany().getEmail();
+                expiredProductsByEmail
+                        .computeIfAbsent(email, k -> new ArrayList<>())
+                        .add(product);
             }
         }
 
+        expiredProductsByEmail.forEach((email, expiredProducts) -> {
+            emailService.sendEmail(
+                    email,
+                    "Produtos vencidos",
+                    "Produtos vencidos: %s".formatted(expiredProducts)
+            );
+        });
     }
 
     @Scheduled(fixedRate = 30000)
     public void setOutOfStockProduct() {
         List<Product> products = productRepository.findAll();
+        Map<String, List<Product>> outOfStockProductsByEmail = new HashMap<>();
 
         for (Product product : products) {
-            if(product.getQuantity() <= product.getWarningQuantity()){
+            if (product.getQuantity() <= product.getWarningQuantity() && !product.getIsSendedToEmail()) {
                 product.setIsOutOfStock(true);
+                product.setIsSendedToEmail(true);
+                String email = product.getCompany().getEmail();
+                outOfStockProductsByEmail
+                        .computeIfAbsent(email, k -> new ArrayList<>())
+                        .add(product);
             }
         }
+
+        outOfStockProductsByEmail.forEach((email, outOfStockProducts) -> {
+            emailService.sendEmail(
+                    email,
+                    "Produtos fora de estoque",
+                    "Os seguintes produtos estão fora de estoque: %s".formatted(outOfStockProducts)
+            );
+        });
     }
 }
