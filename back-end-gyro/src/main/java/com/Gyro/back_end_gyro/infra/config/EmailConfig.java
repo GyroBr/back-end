8cdfbcfb1c9aa87ea641d2f051e45674748f86eb
EduardoMeloDeOliveira <eduardo.moliveira@sptech.school>
2025-05-13 08:49:17 -0300

    corrige function initalizer do java mailer

--- back-end-gyro/src/main/java/com/Gyro/back_end_gyro/infra/config/EmailConfig.java
+++ back-end-gyro/src/main/java/com/Gyro/back_end_gyro/infra/config/EmailConfig.java
@@ -1,15 +1,52 @@
 package com.Gyro.back_end_gyro.infra.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
+import java.util.Properties;
+
 @Configuration
 public class EmailConfig {
 
+    @Value("${SPRING_MAIL_HOST}")
+    private String host;
+
+    @Value("${SPRING_MAIL_PORT}")
+    private int port;
+
+    @Value("${SPRING_MAIL_USERNAME}")
+    private String username;
+
+    @Value("${SPRING_MAIL_PASSWORD}")
+    private String password;
+
+    @Value("${SPRING_MAIL_PROPERTIES_MAIL_SMTP_AUTH}")
+    private String smtpAuth;
+
+    @Value("${SPRING_MAIL_PROPERTIES_MAIL_SMTP_SSL_ENABLE}")
+    private String sslEnable;
+
+    @Value("${SPRING_MAIL_PROPERTIES_MAIL_SMTP_STARTTLS_ENABLE}")
+    private String starttlsEnable;
+
     @Bean
     public JavaMailSender getJavaMailSender() {
-        return new JavaMailSenderImpl();
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.smtp.auth", smtpAuth);
+        props.put("mail.smtp.ssl.enable", sslEnable);
+        props.put("mail.smtp.starttls.enable", starttlsEnable);
+        props.put("mail.transport.protocol", "smtp");
+
+        return mailSender;
     }
 }
