59ccfd43c790c43e4959612cc0da50ae61cf5bf8
EduardoMeloDeOliveira <eduardo.moliveira@sptech.school>
2025-05-25 03:05:15 -0300

    feature: config swagger

--- back-end-gyro/src/main/java/com/Gyro/back_end_gyro/infra/config/SwaggerConfig.java
+++ back-end-gyro/src/main/java/com/Gyro/back_end_gyro/infra/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.Gyro.back_end_gyro.infra.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -10,7 +11,13 @@ import org.springframework.context.annotation.Configuration;
                 title = "Gyro API",
                 version = "v1",
                 description = "Documentação da API Gyro"
-        )
+        ),
+        servers = {
+                @Server(
+                        url = "/api",
+                        description = "Servidor principal (via reverse proxy)"
+                )
+        }
 )
 public class SwaggerConfig {
 }
